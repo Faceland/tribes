625be8c55c6351afc32963a5c565625cc986f26f
Richard Harrah <topplethenunnery@gmail.com>
2015-01-27 01:20:28 -0500

    adding MemberManager

---
+++ src/main/java/me/topplethenun/tribes/managers/MemberManager.java
@@ -0,0 +1,52 @@
+package me.topplethenun.tribes.managers;
+
+import com.google.common.base.Preconditions;
+import me.topplethenun.tribes.data.Member;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemberManager {
+
+    private final Map<UUID, Member> memberMap;
+
+    public MemberManager() {
+        memberMap = new ConcurrentHashMap<>();
+    }
+
+    public void addMember(Member member) {
+        Preconditions.checkNotNull(member);
+        Preconditions.checkState(!memberMap.containsKey(member.getUniqueId()));
+        memberMap.put(member.getUniqueId(), member);
+    }
+
+    public void removeMember(Member member) {
+        Preconditions.checkNotNull(member);
+        Preconditions.checkState(memberMap.containsKey(member.getUniqueId()));
+        memberMap.remove(member.getUniqueId());
+    }
+
+    public void removeMember(UUID uuid) {
+        Preconditions.checkNotNull(uuid);
+        Preconditions.checkState(memberMap.containsKey(uuid));
+        memberMap.remove(uuid);
+    }
+
+    public boolean hasMember(Member member) {
+        Preconditions.checkNotNull(member);
+        return memberMap.containsKey(member.getUniqueId());
+    }
+
+    public boolean hasMember(UUID uuid) {
+        Preconditions.checkNotNull(uuid);
+        return memberMap.containsKey(uuid);
+    }
+
+    public Set<Member> getMembers() {
+        return new HashSet<>(memberMap.values());
+    }
+
+}
